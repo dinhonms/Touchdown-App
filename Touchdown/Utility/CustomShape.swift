5c04156a8d881c76d350317891909f27462209fa
dinhonms <dinhonms@gmail.com>
2023-11-20 16:34:00 -0300

    * detail screen WIP

---
+++ Touchdown/Utility/CustomShape.swift
@@ -0,0 +1,18 @@
+//
+//  CustomShape.swift
+//  Touchdown
+//
+//  Created by Nonato Sousa on 20/11/23.
+//
+
+import SwiftUI
+
+struct CustomShape: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CustomShape()
+}
